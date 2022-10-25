4cb2ebb85b5c3385cdfe58485bf09505e5041ae4
Jack <99660099+JackVoron@users.noreply.github.com>
2022-10-25 20:42:36 +0300

    extra lines related to another parser have been removed

--- BBWikiInjuriesParser.py
+++ BBWikiInjuriesParser.py
@@ -2,37 +2,7 @@ from asyncio.windows_events import NULL
 import base64,requests,os
 from bs4 import BeautifulSoup
 
-CATEGORIES = ["Assault",
-"Attachement",
-"Grenade",
-"LMG",
-"Melee",
-"Misc",
-"Pistol",
-"Rifle",
-"Rocket",
-"Shotgun",
-"SMG",
-"IconAttachement",
-"Ammo",
-"Armor",
-"Bags",
-"Clothes",
-"Eyes",
-"Helmet",
-"Masks",
-"Medicine",
-"Other",
-"Patches",
-"Pouches",
-"Radio",
-"Vest",
-"Food",
-"Combined",
-]
-
 HEADERS = {"user_agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.160 YaBrowser/22.5.4.904 Yowser/2.5 Safari/537.36"}
-
 
 def parse():
         os.mkdir("Injuries") if not os.path.isdir("Injuries") else NULL
